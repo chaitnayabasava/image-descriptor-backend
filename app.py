75a9be1007eae39f6c1723f07595839c90c00219
chaitnayabasava <viswachaitanya.b16@iiits.in>
2020-07-19 17:23:40 +0530

    modified CORS and Dockerfile.

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import request, Flask
 from flask import jsonify
+from flask_cors import CORS, cross_origin
 from PIL import Image
 import io
 
 from predict import get_predictions
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    return response
+cors = CORS(app, resources={r"/predict_petal_length": {"origins": "*"}})
+
+# @app.after_request
+# def after_request(response):
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+#     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+#     return response
 
 @app.route('/image-upload', methods=['POST'])
+@cross_origin(origin='*',headers=['Content- Type'])
 def image_upload():
     r = request.args
     img = Image.open(io.BytesIO(request.files['file'].read())).convert("RGB")
@@ -22,4 +27,4 @@ def image_upload():
     return jsonify({'sentences': sentences})
 
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+    app.run(host='0.0.0.0')
